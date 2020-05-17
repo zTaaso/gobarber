a2739d75594854a86c158398d42ada9892353726
ztaaso <ztaaso.cedaspy@gmail.com>
2020-05-17 01:03:24 -0300

    provider schedule added

--- backend/src/database/index.js
+++ backend/src/database/index.js
@@ -1,4 +1,5 @@
 import Sequelize from 'sequelize';
+import mongoose from 'mongoose';
 
 import User from '../app/models/User';
 import File from '../app/models/File';
@@ -11,6 +12,7 @@ const models = [User, File, Appointment];
 class Database {
     constructor() {
         this.init();
+        this.mongo();
     }
 
     init() {
@@ -23,6 +25,17 @@ class Database {
                     model.associate && model.associate(this.connection.models)
                 );
             });
+    }
+
+    mongo() {
+        this.mongoConnection = mongoose.connect(
+            'mongodb://192.168.0.110:27017/gobarber',
+            {
+                useNewUrlParser: true,
+                useFindAndModify: true,
+                useUnifiedTopology: true,
+            }
+        );
     }
 }
 
