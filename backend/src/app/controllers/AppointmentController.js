5383924725accbca6706e13e43a1c83a01870b8f
ztaaso <ztaaso.cedaspy@gmail.com>
2020-05-16 23:52:57 -0300

    get appointments lists route added

--- backend/src/app/controllers/AppointmentController.js
+++ backend/src/app/controllers/AppointmentController.js
@@ -1,8 +1,37 @@
 import * as Yup from 'yup';
+import { isBefore, parseISO, startOfHour } from 'date-fns';
+
 import Appointment from '../models/Appointment';
 import User from '../models/User';
+import File from '../models/File';
 
 class AppointmentController {
+    async index(req, res) {
+        const appointments = await Appointment.findAll({
+            where: {
+                user_id: req.userId,
+                canceled_at: null,
+            },
+            attributes: ['id', 'date'],
+            include: [
+                {
+                    model: User,
+                    as: 'provider',
+                    attributes: ['id', 'name'],
+                    include: [
+                        {
+                            model: File,
+                            as: 'avatar',
+                            attributes: ['id', 'path', 'url'],
+                        },
+                    ],
+                },
+            ],
+        });
+
+        return res.json(appointments);
+    }
+
     async store(req, res) {
         const schema = Yup.object().shape({
             date: Yup.date().required(),
@@ -17,6 +46,7 @@ class AppointmentController {
 
         const { provider_id, date } = req.body;
 
+        // Checks if the id is from a  provider
         const isProvider = await User.findOne({
             where: {
                 id: provider_id,
@@ -27,11 +57,32 @@ class AppointmentController {
         if (!isProvider) {
             return res
                 .status(401)
-                .json({ err: "The received id is not a provider's one." });
+                .json({ error: "The received id is not a provider's one." });
+        }
+
+        const hourStart = startOfHour(parseISO(date));
+
+        // Check for past dates
+        if (isBefore(hourStart, new Date())) {
+            return res.status(400).json({ error: 'Invalid date' });
+        }
+
+        const checkAvailability = await Appointment.findOne({
+            where: {
+                provider_id,
+                canceled_at: null,
+                date: hourStart,
+            },
+        });
+
+        if (checkAvailability) {
+            return res
+                .status(400)
+                .json({ error: 'Appointment date not avaliable.' });
         }
 
         const appointment = await Appointment.create({
-            date,
+            date: hourStart,
             provider_id,
             user_id: req.userId,
         });
