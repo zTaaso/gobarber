9002c76fc74752ba05d937e41dcf3d8c60271981
zTaaso <ztaaso.cedaspy@gmail.com>
2020-05-25 23:57:30 -0300

    Available edited

--- backend/src/app/controllers/AvailableController.js
+++ backend/src/app/controllers/AvailableController.js
@@ -1,6 +1,31 @@
+import Appointment from '../models/Appointment';
+import { startOfDay, endOfDay } from 'date-fns';
+import { Op } from 'sequelize';
+
 class AvailableController {
     async index(req, res) {
-        return res.json({ oi: 'me chupa' });
+        const { date } = req.query;
+
+        if (!date) {
+            return res.status(400).json({ error: 'Date missing.' });
+        }
+
+        const searchDate = Number(date);
+
+        const appointments = await Appointment.findAll({
+            where: {
+                provider_id: req.params.providerId,
+                canceled_at: null,
+                date: {
+                    [Op.between]: [
+                        startOfDay(searchDate),
+                        endOfDay(searchDate),
+                    ],
+                },
+            },
+        });
+
+        return res.json(appointments);
     }
 }
 
