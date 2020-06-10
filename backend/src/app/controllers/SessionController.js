6f4eff43d9dc6c18d593650f94bffc519f5af1d3
zTaaso <ztaaso.cedaspy@gmail.com>
2020-06-10 13:04:39 -0300

    retifications in api to frontend

--- backend/src/app/controllers/SessionController.js
+++ backend/src/app/controllers/SessionController.js
@@ -3,6 +3,7 @@ import * as Yup from 'yup';
 
 import User from '../models/User';
 import auth from '../../config/auth';
+import File from '../models/File';
 
 class Session {
     async store(req, res) {
@@ -17,7 +18,16 @@ class Session {
 
         const { email, password } = req.body;
 
-        const user = await User.findOne({ where: { email } });
+        const user = await User.findOne({
+            where: { email },
+            include: [
+                {
+                    model: File,
+                    as: 'avatar',
+                    attributes: ['id', 'path', 'url'],
+                },
+            ],
+        });
 
         if (!user) {
             return res.status(401).json({ error: 'User not found.' });
@@ -27,13 +37,15 @@ class Session {
             return res.status(401).json({ error: 'Password does not match.' });
         }
 
-        const { id, name } = user;
+        const { id, name, avatar, provider } = user;
 
         return res.json({
             user: {
                 id,
                 name,
                 email,
+                provider,
+                avatar,
             },
             token: jwt.sign({ id }, auth.secret, {
                 expiresIn: auth.expires,
--- backend/src/app/controllers/UserController.js
+++ backend/src/app/controllers/UserController.js
@@ -1,5 +1,6 @@
 import * as Yup from 'yup';
 import User from '../models/User';
+import File from '../models/File';
 
 class UserController {
     async store(req, res) {
@@ -66,9 +67,19 @@ class UserController {
             return res.status(401).json({ error: 'Password does not match.' });
         }
 
-        const { id, name, provider, avatar_id } = await user.update(req.body);
+        await user.update(req.body);
 
-        return res.json({ id, name, email, provider, avatar_id });
+        const { id, name, provider, avatar } = await User.findByPk(req.userId, {
+            include: [
+                {
+                    model: File,
+                    as: 'avatar',
+                    attributes: ['id', 'path', 'url'],
+                },
+            ],
+        });
+
+        return res.json({ id, name, email, provider, avatar });
     }
 }
 
