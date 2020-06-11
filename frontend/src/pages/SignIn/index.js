e514cab46c32c6e3d1c2511ddfde1c57f8db40fc
zTaaso <ztaaso.cedaspy@gmail.com>
2020-06-10 23:18:25 -0300

    pages layout finished

--- frontend/src/pages/SignIn/index.js
+++ frontend/src/pages/SignIn/index.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Form } from '@unform/web';
+import Input from '~/components/Input';
 
+import logo from '~/assets/logo.svg';
 // import { Container } from './styles';
 
 function SignIn() {
-  return <h1>SignIn</h1>;
+  function handleSubmit(data, { reset }) {
+    reset();
+  }
+
+  return (
+    <>
+      <img src={logo} alt="GoBarber" />
+
+      <Form onSubmit={handleSubmit}>
+        <Input type="email" name="email" placeholder="Seu email" />
+        <Input type="password" name="password" placeholder="Sua senha" />
+
+        <button type="submit">Acessar</button>
+        <Link to="/register">Criar conta gratuita</Link>
+      </Form>
+    </>
+  );
 }
 
 export default SignIn;
--- frontend/src/pages/SignUp/index.js
+++ frontend/src/pages/SignUp/index.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Form } from '@unform/web';
+import Input from '~/components/Input';
 
+import logo from '~/assets/logo.svg';
 // import { Container } from './styles';
 
 function SignUp() {
-  return <h1>SignUp</h1>;
+  function handleSubmit(data, { reset }) {
+    console.tron.log(data);
+    reset();
+  }
+
+  return (
+    <>
+      <img src={logo} alt="GoBarber" />
+
+      <Form onSubmit={handleSubmit}>
+        <Input type="text" name="name" placeholder="Nome completo" />
+        <Input type="email" name="email" placeholder="Seu email" />
+        <Input type="password" name="password" placeholder="Sua senha" />
+
+        <button type="submit">Criar conta</button>
+        <Link to="/register">Já tenho conta</Link>
+      </Form>
+    </>
+  );
 }
 
 export default SignUp;
