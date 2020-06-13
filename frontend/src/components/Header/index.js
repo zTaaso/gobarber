43263930642f636db74c1aae87fd726c86f6fba2
zTaaso <ztaaso.cedaspy@gmail.com>
2020-06-13 00:03:37 -0300

    Header created

---
+++ frontend/src/components/Header/index.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import logo from '~/assets/logo-purple.svg';
+
+import Notifications from '~/components/Notifications';
+import { Container, Content, Profile } from './styles';
+
+function Header() {
+  return (
+    <Container>
+      <Content>
+        <nav>
+          <img src={logo} alt="GoBarber" />
+
+          <Link to="/dashboard">DASHBOARD</Link>
+        </nav>
+
+        <aside>
+          <Notifications />
+
+          <Profile>
+            <div>
+              <strong>Thiago Afonso</strong>
+              <Link to="/profile">Meu perfil</Link>
+            </div>
+            <img
+              src="https://api.adorable.io/avatars/50/abott@adorable.png
+"
+              alt="GoBarber"
+            />
+          </Profile>
+        </aside>
+      </Content>
+    </Container>
+  );
+}
+
+export default Header;
