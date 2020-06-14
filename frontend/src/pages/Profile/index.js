2d19f902ffb8b4c895f0b888ddaf70da7a86d3e2
zTaaso <ztaaso.cedaspy@gmail.com>
2020-06-14 18:10:58 -0300

    Profile html created

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,9 +1,37 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { Form } from '@unform/web';
 
-// import { Container } from './styles';
+import Input from '~/components/Input';
+
+import { Container } from './styles';
 
 function Profile() {
-  return <h1>Profile</h1>;
+  const profile = useSelector((state) => state.user.profile);
+
+  function handleSubmit(data) {}
+  return (
+    <Container>
+      <Form initialData={profile}>
+        <Input name="name" placeholder="Nome completo" />
+        <Input name="email" type="email" placeholder="Endereço de e-mail" />
+
+        <hr />
+
+        <Input name="oldPassword" type="password" placeholder="Senha antiga" />
+        <Input name="password" type="password" placeholder="Nova senha" />
+        <Input
+          name="confirmPassword"
+          type="password"
+          placeholder="Confirmação da nova senha"
+        />
+
+        <button type="submit">Atualizar perfil</button>
+      </Form>
+
+      <button type="button">Sair do GoBarber</button>
+    </Container>
+  );
 }
 
 export default Profile;
