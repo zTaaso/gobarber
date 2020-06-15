f2b29413c008b4e9c1977c8d68f465aa16336a53
zTaaso <ztaaso.cedaspy@gmail.com>
2020-06-15 19:02:59 -0300

    update profile saga created

---
+++ frontend/src/store/modules/user/sagas.js
@@ -0,0 +1,22 @@
+import { put, call, takeLatest, all } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
+import api from '~/services/api';
+import { updateProfileSuccess } from './actions';
+
+export function* updateProfile({ payload }) {
+  try {
+    const { data } = payload;
+    const { email, name, ...rest } = data;
+
+    const profile = { email, name, ...(rest.oldPassword ? rest : {}) };
+
+    const response = yield call(api.put, 'users', profile);
+
+    yield put(updateProfileSuccess(response.data));
+    toast.success('Usuário editado com suecesso!');
+  } catch (err) {
+    toast.error('Algo deu errado. Verifique seus dados e tente novamente. ');
+  }
+}
+
+export default all([takeLatest('@user/UPDATE_PROFILE_REQUEST', updateProfile)]);
