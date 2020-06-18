0f75d6ff0d7da39fda2a698cd2c4d27b42091833
zTaaso <ztaaso.cedaspy@gmail.com>
2020-06-18 00:44:18 -0300

    verify if image is succesfully fetched

--- frontend/src/components/Header/index.js
+++ frontend/src/components/Header/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 
@@ -6,9 +6,23 @@ import logo from '~/assets/logo-purple.svg';
 
 import Notifications from '~/components/Notifications';
 import { Container, Content, Profile } from './styles';
+import api from '~/services/api';
 
 function Header() {
   const profile = useSelector((state) => state.user.profile);
+  const [avatarUrl, setAvatarUrl] = useState('');
+
+  useEffect(() => {
+    async function getAvatar() {
+      try {
+        await api.get(profile.avatar.url);
+        setAvatarUrl(profile.avatar.url);
+      } catch (err) {
+        setAvatarUrl('https://api.adorable.io/avatars/50/abott@adorable.png');
+      }
+    }
+    getAvatar();
+  }, []);
 
   return (
     <Container>
@@ -27,13 +41,7 @@ function Header() {
               <strong>{profile.name}</strong>
               <Link to="/profile">Meu perfil</Link>
             </div>
-            <img
-              src={
-                profile.avatar.url ||
-                'https://api.adorable.io/avatars/50/abott@adorable.png'
-              }
-              alt={profile.name}
-            />
+            <img src={avatarUrl} alt={profile.name} />
           </Profile>
         </aside>
       </Content>
