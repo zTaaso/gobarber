266118b8e075be4713622992f468d5c0ed29c331
zTaaso <ztaaso.cedaspy@gmail.com>
2020-06-20 17:39:22 -0300

    page height fixed

--- frontend/src/pages/_layouts/default/styles.js
+++ frontend/src/pages/_layouts/default/styles.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const Wrapper = styled.div`
   min-height: 100%;
-  padding-bottom: 50px;
+  padding-bottom: 25px;
 
   background: linear-gradient(-90deg, #7159c1, #ab59c1);
 `;
