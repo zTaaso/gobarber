4a7bb0f5162f16085579b310e2fe03cb9a926ff4
zTaaso <ztaaso.cedaspy@gmail.com>
2020-08-01 01:24:52 -0300

    folders moved

--- index.js
+++
@@ -1,9 +0,0 @@
-import { registerRootComponent } from 'expo';
-import 'react-native-gesture-handler';
-
-import App from './src';
-
-// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
-// It also ensures that whether you load the app in the Expo client or in a native build,
-// the environment is set up appropriately
-registerRootComponent(App);
