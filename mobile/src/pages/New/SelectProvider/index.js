ac5a573984996afd0aa9a2c6890741ff42afdf73
zTaaso <ztaaso.cedaspy@gmail.com>
2020-08-06 11:16:18 -0300

    select provider screen created

---
+++ mobile/src/pages/New/SelectProvider/index.js
@@ -0,0 +1,62 @@
+import React, { useState, useEffect } from 'react';
+import { TouchableOpacity } from 'react-native';
+import Icon from '@expo/vector-icons/MaterialIcons';
+
+import api from '~/services/api';
+
+import Background from '~/components/Background';
+
+import { Container, ProvidersList, Provider, Avatar, Name } from './styles';
+
+const SelectProvider = ({ navigation }) => {
+  navigation.setOptions({
+    title: 'Selecione o prestador',
+    headerLeft: () => (
+      <TouchableOpacity onPress={() => navigation.goBack()}>
+        <Icon name="chevron-left" size={20} color="#fff" />
+      </TouchableOpacity>
+    ),
+  });
+
+  const [providers, setProviders] = useState([]);
+
+  useEffect(() => {
+    async function loadProviders() {
+      const response = await api.get('/providers');
+
+      setProviders(response.data);
+    }
+    loadProviders();
+  }, []);
+
+  return (
+    <Background>
+      <Container>
+        <ProvidersList
+          data={providers}
+          keyExtractor={(item) => String(item.id)}
+          renderItem={({ item: provider }) => (
+            <Provider
+              onPress={() =>
+                navigation.navigate('New', {
+                  screen: 'SelectTime',
+                })
+              }
+            >
+              <Avatar
+                source={{
+                  uri: provider.avatar
+                    ? provider.avatar.url
+                    : `https://api.adorable.io/avatars/60/${provider.name}.png`,
+                }}
+              />
+              <Name>{provider.name}</Name>
+            </Provider>
+          )}
+        />
+      </Container>
+    </Background>
+  );
+};
+
+export default SelectProvider;
